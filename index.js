72a0899361cdde7013ef404c57bed4e76a975f79
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-09-05 15:56:52 +0000

    fixed linter errors in the html, css and javascript files

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let toggleBtn = document.querySelector('#toggle-btn')
-let ulElem = document.querySelector('#list-items')
+const toggleBtn = document.querySelector('#toggle-btn');
+const ulElem = document.querySelector('#list-items');
 
 toggleBtn.addEventListener('click', () => {
-  ulElem.style.display = "block"
-})
+  ulElem.style.display = 'block';
+});
 
-let closeButton = document.querySelector('#closeBtn')
+const closeButton = document.querySelector('#closeBtn');
 
 closeButton.addEventListener('click', () => {
-  ulElem.style.display = "none";
-})
+  ulElem.style.display = 'none';
+});
 
-let list = document.querySelectorAll('#list-items li a')
+const list = document.querySelectorAll('#list-items li a');
 
-list.forEach(item => {
+list.forEach((item) => {
   item.addEventListener('click', () => {
-    ulElem.style.display = "none";
-  })
-})
+    ulElem.style.display = 'none';
+  });
+});
