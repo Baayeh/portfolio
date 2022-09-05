9a1fff0b4f5f584a8163c133034e5c4acd69336b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-09-05 18:30:26 +0000

    Made changes to the mobile menu and added some smooth transitions

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const toggleBtn = document.querySelector('#toggle-btn');
-const ulElem = document.querySelector('#list-items');
+const openBtn = document.querySelector('#toggle-btn');
+const closeBtn = document.querySelector('#close-btn');
+const navList = document.querySelector('.nav-list');
+const links = document.querySelectorAll('ul.nav-list li a');
 
-toggleBtn.addEventListener('click', () => {
-  ulElem.style.display = 'block';
+openBtn.addEventListener('click', () => {
+  navList.style.left = '0';
 });
 
-const closeButton = document.querySelector('#closeBtn');
-
-closeButton.addEventListener('click', () => {
-  ulElem.style.display = 'none';
+closeBtn.addEventListener('click', () => {
+  navList.style.left = '-25em';
 });
 
-const list = document.querySelectorAll('#list-items li a');
-
-list.forEach((item) => {
-  item.addEventListener('click', () => {
-    ulElem.style.display = 'none';
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    navList.style.left = '-25em';
+    const href = link.getAttribute('href');
+    const moveTop = document.querySelector(href).offsetTop;
+    setTimeout(() => {
+      window.scroll({
+        top: moveTop,
+        behavior: 'smooth',
+      });
+    }, 700);
   });
 });
