0df06216db899cb033fb86d611025fd7b9a2fe95
til2to <padwusup@.com>
2022-09-06 13:45:51 +0000

    Display project details dynamically

--- index.js
+++ index.js
@@ -25,3 +25,74 @@ links.forEach((link) => {
     }, 700);
   });
 });
+
+const projectWrapper = document.querySelector('.project-list');
+const projectArray = [
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+];
+projectArray.forEach((project) => {
+  let tech = '';
+  project.technologies.forEach((item) => {
+    tech += `<li class="pj-card-list-item">
+    <span>${item}</span>
+    </li>`;
+  });
+  const projectItem = `<div class="pj-card pj-2">
+  <div class="pj-thumbnail"></div>
+  <div class="pj-card-body">
+      <h2 class="pj-card-title">${project.name}</h2>
+      <ul class="pj-card-lists">
+        ${tech}
+      </ul>
+      <button class="pj-card-action" type="button">See Project</button>
+  </div>
+  </div>`;
+  projectWrapper.innerHTML += projectItem;
+});
