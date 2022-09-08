f51fc0954113a3593388847924c5ba81d842417b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-09-08 09:52:58 +0000

    Displayed the individual details of each project in the index.js file

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const openBtn = document.querySelector('#toggle-btn');
 const closeBtn = document.querySelector('#close-btn');
 const navList = document.querySelector('.nav-list');
 const links = document.querySelectorAll('ul.nav-list li a');
+const projectList = document.querySelector('.project-list');
+const modalClose = document.querySelector('.modal_close');
+const modalOverlay = document.querySelector('#proj-overlay');
+const modalContent = document.querySelector('#modal_content');
+const mainWrapper = document.querySelector('.main-wrapper');
 
 openBtn.addEventListener('click', () => {
   navList.style.left = '0';
@@ -26,10 +31,17 @@ links.forEach((link) => {
   });
 });
 
-const projectWrapper = document.querySelector('.project-list');
-const projectArray = [
+const recentProjects = [
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory1',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint iusto voluptas a? Libero dicta ducimus asperiores ea repudiandae illum debitis, beatae animi, aliquam voluptates suscipit rerum placeat! Perferendis ratione molestiae, deleniti rem maiores in totam laboriosam libero odit animi aut.',
+    img: '',
+    technologies: ['css', 'javascript', 'html'],
+    linkDemo: '',
+    sourceCode: '',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory2',
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
     img: '',
     technologies: ['css', 'javascript', 'html'],
@@ -37,7 +49,7 @@ const projectArray = [
     sourceCode: '',
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory3',
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
     img: '',
     technologies: ['css', 'javascript', 'html'],
@@ -45,7 +57,7 @@ const projectArray = [
     sourceCode: '',
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory4',
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
     img: '',
     technologies: ['css', 'javascript', 'html'],
@@ -53,7 +65,7 @@ const projectArray = [
     sourceCode: '',
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory5',
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
     img: '',
     technologies: ['css', 'javascript', 'html'],
@@ -61,15 +73,7 @@ const projectArray = [
     sourceCode: '',
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
-    img: '',
-    technologies: ['css', 'javascript', 'html'],
-    linkDemo: '',
-    sourceCode: '',
-  },
-  {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: 'Multi-Post Stories Gain+Glory6',
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque animi distinctio voluptate. Earum nulla ex, et blanditiis harum iure libero.',
     img: '',
     technologies: ['css', 'javascript', 'html'],
@@ -77,22 +81,75 @@ const projectArray = [
     sourceCode: '',
   },
 ];
-projectArray.forEach((project) => {
-  let tech = '';
-  project.technologies.forEach((item) => {
-    tech += `<li class="pj-card-list-item">
-    <span>${item}</span>
-    </li>`;
+
+recentProjects.forEach((project, index) => {
+  let technologies = '';
+  project.technologies.forEach((technology) => {
+    technologies += `<li class="pj-card-list-item">
+          <span>${technology}</span>
+      </li>`;
   });
-  const projectItem = `<div class="pj-card pj-2">
-  <div class="pj-thumbnail"></div>
-  <div class="pj-card-body">
-      <h2 class="pj-card-title">${project.name}</h2>
-      <ul class="pj-card-lists">
-        ${tech}
-      </ul>
-      <button class="pj-card-action" type="button">See Project</button>
-  </div>
+  const projectItem = ` <div class="pj-card pj-1" id=${index}>
+    <div class="pj-thumbnail"></div>
+    <div class="pj-card-body">
+        <h2 class="pj-card-title">${project.name}</h2>
+        <ul class="pj-card-lists">
+          ${technologies}
+        </ul>
+        <button class="pj-card-action" type="button">See Project</button>
+    </div>
   </div>`;
-  projectWrapper.innerHTML += projectItem;
+
+  projectList.innerHTML += projectItem;
+});
+
+const projectDetails = document.querySelectorAll('.pj-card-action');
+
+// Modal Pop up
+modalClose.addEventListener('click', () => {
+  modalOverlay.style.display = 'none';
+  mainWrapper.style.filter = 'blur(0)';
+});
+
+const addModalContent = (projItem) => {
+  let technologies = '';
+  projItem.technologies.forEach((technology) => {
+    technologies += `<li class="pj-tech-item">
+    <span>${technology}</span>
+</li>`;
+  });
+  const modDetail = `<div class="proj_img">
+                      <img src="./assets/Snapshoot_Portfolio.png" width="100%" alt="">
+                    </div>
+
+                    <div class="content-info">
+                      <h2 class="proj-name">${projItem.name}</h2>
+                      <div class="content-info-btn">
+                          <a href=${projItem.linkDemo} type="button" class="info-link">
+                              <span>See live</span>
+                              <span><img src="./assets/Icon-Export.svg" alt=""></span>
+                          </a>
+                          <a href=${projItem.sourceCode} type="button" class="info-link">
+                              <span>See Source</span>
+                              <span><img src="./assets/Icon-GitHub.svg" alt=""></span>
+                          </a>
+                      </div>
+                      <ul class="pj-tech-lists">
+                        ${technologies}
+                      </ul>
+                      <p class="proj-desc">${projItem.description}</p>
+                    </div>`;
+  modalContent.innerHTML = modDetail;
+  modalOverlay.style.display = 'block';
+  mainWrapper.style.filter = 'blur(14px)';
+  modalContent.scrollTop = 0;
+};
+
+projectDetails.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    console.log(e);
+    const singleProj = e.target.parentElement.parentElement.getAttribute('id');
+    const project = recentProjects[singleProj];
+    addModalContent(project);
+  });
 });
